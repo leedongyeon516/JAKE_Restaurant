6400110bb110bc5456f30a3a6fab860bb21c198e
JAKE LEE <lee@JAKEs-MacBook-Pro.local>
2019-10-10 14:19:32 -0400

    Updates

--- js/loader.js
+++ js/loader.js
@@ -6,6 +6,7 @@ const loaderLink = document.querySelector(".view-btn");
 function init() {
   setTimeout(() => {
     loaderMain.style.display = "block";
+
     setTimeout(() => (loader.style.opacity = 0), 50);
     setTimeout(() => (loaderMain.style.opacity = 1), 50);
   }, 1500);
